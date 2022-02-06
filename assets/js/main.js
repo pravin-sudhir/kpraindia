817d2bc6696514253490941c8fde8454cef3d609
Pravin Kumar Singh <sudhir@skugal.com>
2022-02-06 11:27:29 +0530

    trading

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,4 +70,23 @@ $(document).ready(function () {
             }
         }
     });
+
+    $("#management_Slider").owlCarousel({
+        margin:20,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 })
