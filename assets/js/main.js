7bc046e6d389926097043989abc2aa7dba50357f
Pravin Kumar Singh <sudhir@skugal.com>
2022-02-16 09:06:55 +0530

    corporate-responsibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,6 +108,24 @@ $(document).ready(function () {
             }
         }
     });
+    $("#information_Slider").owlCarousel({
+        margin:20,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 
 $(window).scroll(function(){if ($(this).scrollTop() >= 50){$('#return-to-top').fadeIn(200);} else{$('#return-to-top').fadeOut(200);}}); 
 $('#return-to-top').click(function(){$('body,html').animate({scrollTop : 0 }, 500);});  
